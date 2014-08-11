facaedf67e4687e3922113d6e6d56384cb9862b0
folger <folger@originlab.com.cn>
2014-08-11 11:38:24 +0800

    FindSymbol.py use simple str.find but re

--- mystuff/FindSymbol.py
+++ mystuff/FindSymbol.py
@@ -1,18 +1,24 @@
 import os
-import re
 import sys
 
 import inspect
 currentpath = os.path.dirname(inspect.getfile(inspect.currentframe()))
 
-patterns = [''] + sys.argv[1].split() + ['']
-pattern = re.compile('(\w|:)*'.join(patterns), re.I)
+patterns = [s.lower() for s in sys.argv[1].split()]
+
+def match(name):
+    global patterns
+    name = name.lower()
+    for pattern in patterns:
+        if name.find(pattern) < 0:
+            return False
+    return True
 
 with open(os.path.join(currentpath, 'tags')) as f:
     for line in f:
         entries = line.split(';"')
         name = entries[0].split('\t')[0]
-        if pattern.match(name):
+        if match(name):
             if entries[1].find('class:') > 0 and name.find('::') < 0:
                 continue
             print(entries[0])
