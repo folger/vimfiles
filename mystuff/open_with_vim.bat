14ea0b0343a0303771e8033f445ea75edd628be3
folger <folger@originlab.com.cn>
2015-03-20 11:08:25 +0800

    add shortcut key for "Open with &Vim"

--- mystuff/open_with_vim.bat
+++ mystuff/open_with_vim.bat
@@ -2,12 +2,12 @@
 SET vimPath=G:\vim_min\vim74\gvim.exe
  
 rem add it for all file types
-@reg add "HKEY_CLASSES_ROOT\*\shell\Open with Vim"         /t REG_SZ /v "" /d "Open with Vim"   /f
+@reg add "HKEY_CLASSES_ROOT\*\shell\Open with Vim"         /t REG_SZ /v "" /d "Open with &Vim"   /f
 @reg add "HKEY_CLASSES_ROOT\*\shell\Open with Vim"         /t REG_EXPAND_SZ /v "Icon" /d "%vimPath%,0" /f
 @reg add "HKEY_CLASSES_ROOT\*\shell\Open with Vim\command" /t REG_SZ /v "" /d "%vimPath% \"%%1\"" /f
  
 rem add it for folders
-@reg add "HKEY_CLASSES_ROOT\Folder\shell\Open with Vim"         /t REG_SZ /v "" /d "Open with Vim"   /f
+@reg add "HKEY_CLASSES_ROOT\Folder\shell\Open with Vim"         /t REG_SZ /v "" /d "Open with &Vim"   /f
 @reg add "HKEY_CLASSES_ROOT\Folder\shell\Open with Vim"         /t REG_EXPAND_SZ /v "Icon" /d "%vimPath%,0" /f
 @reg add "HKEY_CLASSES_ROOT\Folder\shell\Open with Vim\command" /t REG_SZ /v "" /d "%vimPath% \"%%1\"" /f
 pause
