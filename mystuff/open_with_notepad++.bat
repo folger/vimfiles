3fce432fa94ea641dfc6d0d4be89bfd06b31ecf0
folger <folger@originlab.com.cn>
2015-12-31 22:09:02 +0800

    open_with_notepad++

---
+++ mystuff/open_with_notepad++.bat
@@ -0,0 +1,13 @@
+@echo off
+SET nppath=D:\Notepad++\Notepad++.exe
+ 
+rem add it for all file types
+@reg add "HKEY_CLASSES_ROOT\*\shell\Open with Notepad++"         /t REG_SZ /v "" /d "Open with Notepad+&="   /f
+@reg add "HKEY_CLASSES_ROOT\*\shell\Open with Notepad++"         /t REG_EXPAND_SZ /v "Icon" /d "%nppath%,0" /f
+@reg add "HKEY_CLASSES_ROOT\*\shell\Open with Notepad++\command" /t REG_SZ /v "" /d "%nppath% \"%%1\"" /f
+ 
+rem add it for folders
+@reg add "HKEY_CLASSES_ROOT\Folder\shell\Open with Notepad++"         /t REG_SZ /v "" /d "Open with Notepad+&="   /f
+@reg add "HKEY_CLASSES_ROOT\Folder\shell\Open with Notepad++"         /t REG_EXPAND_SZ /v "Icon" /d "%nppath%,0" /f
+@reg add "HKEY_CLASSES_ROOT\Folder\shell\Open with Notepad++\command" /t REG_SZ /v "" /d "%nppath% \"%%1\"" /f
+pause
